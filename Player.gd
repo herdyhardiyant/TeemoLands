88801716ef2b4ff5488f268b3e28cbfa84630399
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2024-01-14 11:08:26 +0700

    walk and idle animation

--- Player.gd
+++ Player.gd
@@ -6,17 +6,27 @@ const SPEED = 300.0
 
 @onready var anim = $AnimatedSprite2D
 
-func _ready() -> void:
-	anim.play("walk_south")	
+var nextIdle:String
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("up"):
+		anim.play("walk_up")
 		position.y -= SPEED * delta
-	if Input.is_action_pressed("down"):
+		nextIdle = "idle_up"
+	elif Input.is_action_pressed("down"):
 		position.y += SPEED * delta
-	if Input.is_action_pressed("left"):
+		anim.play("walk_down")
+		nextIdle = "idle_down"		
+	elif Input.is_action_pressed("left"):
 		position.x -= SPEED * delta
-	if Input.is_action_pressed("right"):
+		anim.play("walk_left")
+		nextIdle = "idle_left"		
+	elif Input.is_action_pressed("right"):
 		position.x += SPEED * delta
+		anim.play("walk_right")
+		nextIdle = "idle_right"
+	else:
+		anim.play(nextIdle)
 	
+		
 	
