94c6f5dd4e25ac45883a216c7e27eb828d82b090
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2024-01-24 22:34:52 +0700

    collision ground

--- scripts/fruit.gd
+++ scripts/fruit.gd
@@ -1,17 +1,13 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+var is_ready_to_pickup = false
 func _on_area_2d_body_entered(body):
+	if !is_ready_to_pickup:
+		return
 	if "Player" in body.name:
 		print("Player gets the fruit")
 		queue_free()
+
+
+func _on_timer_timeout():
+	is_ready_to_pickup = true
