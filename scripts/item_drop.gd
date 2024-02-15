bb679081484cebcfc326047f8e7188b55b7e6efd
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2024-02-15 12:27:44 +0700

    fix bug and refactor item drop

--- scripts/item_drop.gd
+++ scripts/item_drop.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 @onready var character_player = $/root/World/CharacterPlayer
 
+
+
 var is_timeout = false
 func _process(_delta):
 	if !is_timeout:
@@ -11,13 +13,20 @@ func _process(_delta):
 	var player_position = character_player.global_position
 	var tween = get_tree().create_tween()
 	tween.tween_property(self,'global_position', player_position, 0.5)
+	var dist_to_player = global_position.distance_to(player_position)
+	if dist_to_player <= 5.0:
+		queue_free()
+	
+	
+	
 
 func _on_timer_timeout():
 	is_timeout = true
 
 func _on_area_2d_body_entered(body):
-	if !is_timeout:
-		return
-		
-	if body is Player:
-		queue_free()
+	pass
+	#if !is_timeout:
+		#return
+		#
+	#if body is Player:
+		#queue_free()
